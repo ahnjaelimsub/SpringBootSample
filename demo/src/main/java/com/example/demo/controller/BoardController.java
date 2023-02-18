97a79e1f0933c1b2c192245cb1b9957b6dddc0d7
안재림 <122592219+Ahnjaelim@users.noreply.github.com>
2023-02-18 15:19:13 +0900

    CRUD 완성

--- demo/src/main/java/com/example/demo/controller/BoardController.java
+++ demo/src/main/java/com/example/demo/controller/BoardController.java
@@ -1,9 +1,14 @@
 package com.example.demo.controller;
 
+import javax.validation.Valid;
+
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 import com.example.demo.dto.BoardDTO;
 import com.example.demo.dto.PageRequestDTO;
@@ -11,10 +16,12 @@ import com.example.demo.dto.PageResponseDTO;
 import com.example.demo.service.BoardService;
 
 import lombok.RequiredArgsConstructor;
+import lombok.extern.log4j.Log4j2;
 
 @Controller
 @RequestMapping("/board")
 @RequiredArgsConstructor
+@Log4j2
 public class BoardController {
 	
 	private final BoardService boardService;
@@ -24,9 +31,58 @@ public class BoardController {
 		PageResponseDTO<BoardDTO> responseDTO = boardService.list(pageRequestDTO);
 		System.out.println(responseDTO);
 		model.addAttribute("responseDTO", responseDTO);
-		model.addAttribute("msg","msgdkfjdkfjl");
-		
-		System.out.println("하");
-		
+	}
+	@GetMapping("/register")
+	public void registerGET(Long wrId, Model model) {
+		if(wrId==null) {
+			log.info("<Board Controller> register GET");
+		}else {
+			log.info("<Board Controller> modify GET");
+			BoardDTO boardDTO = boardService.readOne(wrId);
+			log.info(boardDTO);
+			model.addAttribute("dto", boardDTO);	
+		}
+	}
+	@PostMapping("/modify")
+	public String modifyPOST(PageRequestDTO pageRequestDTO, @Valid BoardDTO boardDTO, BindingResult bindingResult, RedirectAttributes redirectAttributes) {
+		log.info("<Board Controller> modify POST");
+		if(bindingResult.hasErrors()) {
+			log.info("has errors!");
+			String link = pageRequestDTO.getLink();
+			redirectAttributes.addFlashAttribute("errors", bindingResult.getAllErrors());
+			redirectAttributes.addAttribute("wrId", boardDTO.getWrId());
+			return "redirect:/board/register?"+link;
+		}
+		boardService.modify(boardDTO);
+		redirectAttributes.addAttribute("result", "modified");
+		redirectAttributes.addAttribute("wrId", boardDTO.getWrId());
+		return "redirect:/board/read";
+	}
+	@PostMapping("/register")
+	public String registerPost(@Valid BoardDTO boardDTO, BindingResult bindingResult, RedirectAttributes redirectAttributes) {
+		log.info("<Board Controller> register Post");
+		if(bindingResult.hasErrors()) {
+			log.info("has errors!");
+			redirectAttributes.addFlashAttribute("errors", bindingResult.getAllErrors());
+			log.info(bindingResult.getAllErrors());
+			return "redirect:/board/register";
+		}
+		log.info(boardDTO);
+		Long wrId = boardService.register(boardDTO);
+		redirectAttributes.addFlashAttribute("result", wrId);
+		return "redirect:/board/list";
+	}
+	@GetMapping("/read")
+	public void read(Long wrId, PageRequestDTO pageRequestDTO, Model model) { // 변수명 대소문자 구분 주의
+		BoardDTO boardDTO = boardService.readOne(wrId);
+		log.info(boardDTO);
+		model.addAttribute("dto", boardDTO);	
+	}
+	@PostMapping("/remove")
+	public String remove(Long wrId, RedirectAttributes redirectAttributes) {
+		log.info("remove post wrId : "+wrId);
+		boardService.remove(wrId);
+		redirectAttributes.addFlashAttribute("result", "removed");
+		return "redirect:/board/list";
 	}
 }
--- demo/src/main/java/com/example/demo/dto/BoardDTO.java
+++ demo/src/main/java/com/example/demo/dto/BoardDTO.java
@@ -2,6 +2,9 @@ package com.example.demo.dto;
 
 import java.sql.Timestamp;
 
+import javax.validation.constraints.NotEmpty;
+import javax.validation.constraints.Size;
+
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
@@ -13,9 +16,15 @@ import lombok.NoArgsConstructor;
 @NoArgsConstructor
 public class BoardDTO {
 	private Long wrId;
+	
+	@NotEmpty
+	@Size(min=2, max=200)
 	private String wr_subject;
+	@NotEmpty
 	private String wr_content;
+	@NotEmpty
 	private String wr_name;
+	@NotEmpty
 	private String wr_password;
 	private Timestamp wr_datetime;
 	private Timestamp wr_last;
